0261fb71e0548c278f0f230c974cb31202c8bcc8
Aleffelisberto <aleffelisberto@gmail.com>
2023-02-18 18:17:41 -0300

    feat(products-fetch-function): add distributed tracing logs

--- lambda/products/products-fetch-function.ts
+++ lambda/products/products-fetch-function.ts
@@ -2,6 +2,10 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   const method = event.httpMethod;
+  // getting variables for distributed tracing (logs)
+  const lambdaRequestId = context.awsRequestId;
+  const apiRequestId = event.requestContext.requestId;
+  console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
   if (event.resource === "/products" && method === "GET") {
     console.log("GET /products");
     return {
