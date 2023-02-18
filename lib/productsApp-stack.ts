597f1266a477e6135bbe6d13177cea5077dd9855
Aleffelisberto <aleffelisberto@gmail.com>
2023-02-18 17:41:21 -0300

    feat(productsApp-stack): add product lambda function stack

---
+++ lib/productsApp-stack.ts
@@ -0,0 +1,28 @@
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class ProductsAppStack extends cdk.Stack {
+  private readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(
+      this,
+      "productsFetchFunction",
+      {
+        functionName: "ProductsFetchFunction",
+        entry: "lambda/products/products-fetch-function.ts",
+        handler: "handler",
+        memorySize: 128, // in megabytes
+        timeout: cdk.Duration.seconds(2),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+        },
+      }
+    );
+  }
+}
