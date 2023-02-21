81955dfd6482ef635c410995253b2d57f315912b
Aleffelisberto <aleffelisberto@gmail.com>
2023-02-21 12:38:47 -0300

    feat(productsApp-stack): add dynamodb products table and products admin lambda function

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -2,13 +2,27 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class ProductsAppStack extends cdk.Stack {
   public readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
+  public readonly productsAdminHandler: lambdaNodeJS.NodejsFunction;
+  public readonly productsDdb: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
+      tableName: "products",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: id,
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
     this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "productsFetchFunction", {
       functionName: "ProductsFetchFunction",
       entry: "lambda/products/products-fetch-function.ts",
@@ -19,6 +33,25 @@ export class ProductsAppStack extends cdk.Stack {
         minify: true,
         sourceMap: false,
       },
+      environment: {
+        PRODUCTS_DDB: this.productsDdb.tableName,
+      },
     });
+    this.productsDdb.grantReadData(this.productsFetchHandler);
+    this.productsAdminHandler = new lambdaNodeJS.NodejsFunction(this, "productsAdminFunction", {
+      functionName: "ProductsAdminFunction",
+      entry: "lambda/products/products-admin-function.ts",
+      handler: "handler",
+      memorySize: 128, // in megabytes
+      timeout: cdk.Duration.seconds(2),
+      bundling: {
+        minify: true,
+        sourceMap: false,
+      },
+      environment: {
+        PRODUCTS_DDB: this.productsDdb.tableName,
+      },
+    });
+    this.productsDdb.grantWriteData(this.productsAdminHandler);
   }
 }
