aae95647b3fc7af14d093ab081c2657a071b5cb3
Aleffelisberto <aleffelisberto@gmail.com>
2023-02-22 22:43:10 -0300

    feat(products-fetch-function): add GET /products/id route

--- lambda/products/products-fetch-function.ts
+++ lambda/products/products-fetch-function.ts
@@ -1,17 +1,25 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-  const method = event.httpMethod;
   // getting variables for distributed tracing (logs)
   const lambdaRequestId = context.awsRequestId;
   const apiRequestId = event.requestContext.requestId;
   console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
-  if (event.resource === "/products" && method === "GET") {
+  if (event.resource === "/products") {
     console.log("GET /products");
     return {
       statusCode: 200,
       body: JSON.stringify({
         message: "GET products - OK",
+      }),
+    };
+  } else if (event.resource === "/products/{id}") {
+    const id = event.pathParameters!.id as string;
+    console.log(`GET /products/${id}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `GET /products/${id}`,
       }),
     };
   }
