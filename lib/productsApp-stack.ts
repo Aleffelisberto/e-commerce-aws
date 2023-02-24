bf5ce53b3119bf04ba8355ef56773af35b50424b
Aleffelisberto <aleffelisberto@gmail.com>
2023-02-23 23:16:55 -0300

    feat(productsApp-stack): attach productsAppLayer in productApp stack

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -3,6 +3,7 @@ import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 export class ProductsAppStack extends cdk.Stack {
   public readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
@@ -23,6 +24,11 @@ export class ProductsAppStack extends cdk.Stack {
       readCapacity: 1,
       writeCapacity: 1,
     });
+
+    // Products Layer
+    const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn");
+    const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn);
+
     this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "productsFetchFunction", {
       functionName: "ProductsFetchFunction",
       entry: "lambda/products/products-fetch-function.ts",
@@ -36,8 +42,10 @@ export class ProductsAppStack extends cdk.Stack {
       environment: {
         PRODUCTS_DDB: this.productsDdb.tableName,
       },
+      layers: [productsLayer],
     });
     this.productsDdb.grantReadData(this.productsFetchHandler);
+
     this.productsAdminHandler = new lambdaNodeJS.NodejsFunction(this, "productsAdminFunction", {
       functionName: "ProductsAdminFunction",
       entry: "lambda/products/products-admin-function.ts",
@@ -51,6 +59,7 @@ export class ProductsAppStack extends cdk.Stack {
       environment: {
         PRODUCTS_DDB: this.productsDdb.tableName,
       },
+      layers: [productsLayer],
     });
     this.productsDdb.grantWriteData(this.productsAdminHandler);
   }
