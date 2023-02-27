8d61affde00e8e008322830f118f0dbe78bd7e35
Aleffelisberto <aleffelisberto@gmail.com>
2023-02-27 07:14:03 -0300

    feat(product-repository): add create, delete and update methods

--- lambda/products/layers/products-layer/nodejs/product-repository.ts
+++ lambda/products/layers/products-layer/nodejs/product-repository.ts
@@ -9,6 +9,9 @@ export interface Product {
   model: string;
 }
 
+export type ProductCreate = Omit<Product, "id">;
+export type ProductUpdate = Partial<ProductCreate>;
+
 export class ProductRepository {
   private ddbClient: DocumentClient;
   private productsDdb: string;
@@ -26,5 +29,50 @@ export class ProductRepository {
   async findById(id: string): Promise<Product | undefined> {
     const data = await this.ddbClient.get({ TableName: this.productsDdb, Key: { id } }).promise();
     return (data.Item as Product) || undefined;
+  }
+
+  async create(product: ProductCreate): Promise<Product> {
+    const productData = { ...product, id: uuid() };
+    await this.ddbClient
+      .put({
+        TableName: this.productsDdb,
+        Item: productData,
+      })
+      .promise();
+    return productData;
+  }
+
+  async delete(id: string): Promise<Product | undefined> {
+    const product = await this.ddbClient
+      .delete({
+        TableName: this.productsDdb,
+        Key: { id },
+        ReturnValues: "ALL_OLD",
+      })
+      .promise();
+    return (product.Attributes as Product) || undefined;
+  }
+
+  async update(id: string, product: ProductUpdate): Promise<Product | Error> {
+    try {
+      const data = await this.ddbClient
+        .update({
+          TableName: this.productsDdb,
+          Key: { id },
+          ConditionExpression: "attribute_exists(id)",
+          ReturnValues: "UPDATED_NEW",
+          UpdateExpression: "set productName = :n, code = :c, price = :p, model = :m",
+          ExpressionAttributeValues: {
+            ":n": product.productName,
+            ":c": product.code,
+            ":p": product.price,
+            ":m": product.model,
+          },
+        })
+        .promise();
+      return { ...data.Attributes, id } as Product;
+    } catch (err: unknown) {
+      return err as Error;
+    }
   }
 }
