24328001ac256b5f4d0eb8ceaf1cf876b538700d
Aleffelisberto <aleffelisberto@gmail.com>
2023-02-27 07:15:02 -0300

    feat(product-fetch-function): handle requests with productRepository operations

--- lambda/products/products-fetch-function.ts
+++ lambda/products/products-fetch-function.ts
@@ -1,4 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ProductRepository } from "/opt/nodejs/products-layer";
+import { DynamoDB } from "aws-sdk";
+
+const productsDdb = process.env.PRODUCTS_DDB!;
+const ddbClient = new DynamoDB.DocumentClient();
+const productRepository = new ProductRepository(ddbClient, productsDdb);
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   // getting variables for distributed tracing (logs)
@@ -7,21 +13,46 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
   console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
   if (event.resource === "/products") {
     console.log("GET /products");
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "GET products - OK",
-      }),
-    };
+    try {
+      const products = await productRepository.findAll();
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: JSON.stringify(products),
+        }),
+      };
+    } catch (err: unknown) {
+      const message = (<Error>err).message;
+      console.error(message);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message }),
+      };
+    }
   } else if (event.resource === "/products/{id}") {
     const id = event.pathParameters!.id as string;
     console.log(`GET /products/${id}`);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `GET /products/${id}`,
-      }),
-    };
+    try {
+      const product = await productRepository.findById(id);
+      if (!product) {
+        console.error("Product not found");
+        return {
+          statusCode: 404,
+          body: "Product not found",
+        };
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: product }),
+      };
+    } catch (err: unknown) {
+      const message = (<Error>err).message;
+      console.error(message);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message }),
+      };
+    }
   }
   return {
     statusCode: 400,
